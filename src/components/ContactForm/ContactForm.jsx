9044f2036c8850fb4c3466b16be6e8247ccba361
AnnaYakovenko29 <125920571+AnnaYakovenko29@users.noreply.github.com>
2023-06-13 17:19:37 +0300

    goit-react-hw-07-phonebook

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -4,18 +4,18 @@ import { fetchAddContact } from '../../Redux/Contacts/contact-operations';
 
 export default function Form () {
   const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
+  const [phone, setPhone] = useState('');
   const dispatch = useDispatch();
 
   const formSumbit = e => {
     e.preventDefault();
-    dispatch(fetchAddContact({ name, number }));
+    dispatch(fetchAddContact({ name, phone }));
     reset();
   };
 
   const reset = () => {
     setName('');
-    setNumber('');
+    setPhone('');
   };
 
   return (
@@ -35,15 +35,15 @@ export default function Form () {
         />
       </label>
       <label>
-        <span>Number</span>
+        <span>Phone</span>
         <input
           type="tel"
-          name="number"
-          value={number}
+          name="phone"
+          value={phone}
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          title="Phone phone must be digits and can contain spaces, dashes, parentheses and can start with +"
           onChange={e => {
-            setNumber(e.target.value);
+            setPhone(e.target.value);
           }}
           required
         />
